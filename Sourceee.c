e7da1c58ae599a3038314dd58870a3d90a0ac758
HiLMadara <71180913+HiLMadara@users.noreply.github.com>
2020-10-11 20:48:14 +0300

    Add files via upload

--- Sourceee.c
+++ Sourceee.c
@@ -1,30 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int myAtoi(char* str)
+{
+	int sign = 1, result = 0;
+
+	if (*str == '-')
+		sign = -1, str++;
+
+	for (; *str >= '0' && *str <= '9' && *str; str++)
+		result = result * 10 + *str - '0';
+
+	return result * sign;
+}
+
 int main()
 {
-	long int x;
+	int x = 0;
 	long y = 11;
-	int i = 0;
-	int ans = 0;
-	int a[11];
+	long long i = 0;
+	long long ans = 0;
+	long long a[11];
 	printf("Input number x: ");
-	int r;
-	scanf_s("%d", &x);
+	char str[50] = "";
+	fgets(str, 50, stdin);// scanf(" %lli", &x); getchar();
+	for (int i = 0; str[i] != '\n'; i++)
+		if (str[i] < '0' || str[i]>'9') return 1;
+	x = myAtoi(str);
+	printf("%d\n", x);
 	a[0] = 0;
 	a[1] = x;
     a[2] = x * x;
-  for (i = 3; i <= 10; i++) {
-	  if (i % 2 > 0) {
-		  a[i] = a[i - 1] * a[1];
-	  }
-	  else { 
-		  a[i] = a[i / 2] * a[i / 2]; }
-  }
-  for (i = 1; i <= 11; i++) {
-	  ans = ans + i * a[11 - i];
-  }
-  ans = ans + 11; 
-	printf("Answer =  %d\n", ans);
-	return 0;
+	if (a[2] > 0) {
+		for (i = 3; i <= 10; i++) {
+			if (i % 2 > 0) {
+				a[i] = a[i - 1] * a[1];
+			}
+			else {
+				a[i] = a[i / 2] * a[i / 2];
+			}
+		}
+
+		for (i = 1; i <= 11; i++) {
+			ans = ans + i * a[11 - i];
+		}
+		ans = ans + y;
+		printf("Answer =  %lli\n", ans);
+		return 0;
+	}
+	else
+	{
+		printf("u so stupid u cant print number");
+		return 0;
+
+	}
 }
